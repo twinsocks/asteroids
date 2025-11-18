821d1cc1714d93e263ca42c5328e68b97bc55d33
twinsocks <twinsocks@gmail.com>
2025-11-18 16:27:30 +1100

    add player.py, player is triangle

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     dt = 0
 
     while 1 == 1:
@@ -17,12 +19,12 @@ def main():
                 return
             
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        
+        #limits to 60 FPS
         dt = clock.tick(60) / 1000
-        print(dt)
         
-        
-
 
 if __name__ == "__main__":
     main()
