d61ee83559d64e484c5bc100f9684a9b0f93ef1e
twinsocks <twinsocks@gmail.com>
2025-11-21 16:47:14 +1100

    player collision ends game

--- circleshape.py
+++ circleshape.py
@@ -19,3 +19,6 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # must override
         pass
+
+    def collides_with(self, other):
+        return pygame.math.Vector2.distance_to(self.position, other.position) <= self.radius + other.radius
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import sys
 import pygame
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
 
 
@@ -32,7 +33,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        updatable.update(dt)    
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit(0)
         
         screen.fill("black")
         
