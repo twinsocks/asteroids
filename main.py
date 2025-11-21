5460c54fbf324b6b5e593c7b203719f47795e02d
twinsocks <twinsocks@gmail.com>
2025-11-21 12:07:38 +1100

    added the moving asteroids

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 from player import Player
@@ -8,7 +10,19 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
+    
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    asteroidfield = AsteroidField()
+
     dt = 0
 
     while 1 == 1:
@@ -17,10 +31,15 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+
+        updatable.update(dt)    
+        
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        
+        for sprite in drawable:
+            sprite.draw(screen)
+        
+           
         pygame.display.flip()
         
         #limits to 60 FPS
