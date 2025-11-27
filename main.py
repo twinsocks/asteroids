c8b7bd7fc0a14c7329768c32bed461c0ee607a50
twinsocks <twinsocks@gmail.com>
2025-11-28 08:41:20 +1100

    bullets kill shot and asteroid

--- main.py
+++ main.py
@@ -43,6 +43,12 @@ def main():
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit(0)
+            
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    asteroid.kill()
+                    shot.kill()
         
         screen.fill("black")
         
